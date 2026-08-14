76119da7bcbeff6794b635fd2cbd3e6cf3815cbe
ibrahimuphus1-byte <ibrahim.uphus.1@gmail.com>
2026-08-14 13:26:39 +0330

    Update goodies.sh
    Signed-off-by: ibrahimuphus1-byte <ibrahim.uphus.1@gmail.com>

--- scripts/goodies.sh
+++ scripts/goodies.sh
@@ -4,14 +4,10 @@ echo "- Setting up additional goodies..."
 # Clone goodies repository
 git clone https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd.git scripts/goodies/assets &> /dev/null
 # Reset goodies repository to a specific commit
-# Latest commit have iissues so we need to revert it back the janky way.
+# Latest commit have issues so we need to revert it back the janky way.
 # cd scripts/goodies/assets
 # git reset --hard 75f1699a7f0a270ecae7ba696315e3d55d643101 &> /dev/null
 # cd ../../../
-
-# KernelSU
-#chmod +x scripts/goodies/kernelsu.sh
-#source scripts/goodies/kernelsu.sh
 
 # Baseband Guard
 chmod +x scripts/goodies/baseband.sh
