ead4974128a0d519589098e96ebb50391bcf63ee
ibrahimuphus1-byte <ibrahim.uphus.1@gmail.com>
2026-08-16 14:34:50 +0330

    Update compile.sh
    Signed-off-by: ibrahimuphus1-byte <ibrahim.uphus.1@gmail.com>

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,6 +4,7 @@
 echo "-- Applying O3 flags before compiling..."
 sed -i 's/KBUILD_CFLAGS\s\++= -O2/KBUILD_CFLAGS   += -O3/g' Makefile
 sed -i 's/LDFLAGS\s\++= -O2/LDFLAGS += -O3/g' Makefile
+touch .scmversion
 
 # Make sure out folder exist
 mkdir -p out &> /dev/null
