20da4e2c3d6c2b7fdc05982f5c788128b69ef496
ibrahimuphus1-byte <ibrahim.uphus.1@gmail.com>
2026-08-18 08:46:11 +0330

    Update goodies.sh
    Signed-off-by: ibrahimuphus1-byte <ibrahim.uphus.1@gmail.com>

--- scripts/goodies.sh
+++ scripts/goodies.sh
@@ -18,12 +18,12 @@ chmod +x scripts/goodies/nomount.sh
 source scripts/goodies/nomount.sh
 
 # Droidspaces
-chmod +x scripts/goodies/droidspaces.sh
-source scripts/goodies/droidspaces.sh
+#chmod +x scripts/goodies/droidspaces.sh
+#source scripts/goodies/droidspaces.sh
 
 # ReKernel
-chmod +x scripts/goodies/rekernel.sh
-source scripts/goodies/rekernel.sh
+#chmod +x scripts/goodies/rekernel.sh
+#source scripts/goodies/rekernel.sh
 
 # ZRAM
 chmod +x scripts/goodies/zram.sh
