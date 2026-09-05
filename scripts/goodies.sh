c7728b25a0322336d51c5bda081abe39f508145e
ibrahimuphus1-byte <ibrahim.uphus.1@gmail.com>
2026-09-05 07:09:37 +0330

    Update goodies.sh
    Signed-off-by: ibrahimuphus1-byte <ibrahim.uphus.1@gmail.com>

--- scripts/goodies.sh
+++ scripts/goodies.sh
@@ -5,8 +5,8 @@ echo "- Setting up additional goodies..."
 git clone https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd.git scripts/goodies/assets &> /dev/null
 
 # KernelSU
-chmod +x scripts/goodies/kernelsu.sh
-source scripts/goodies/kernelsu.sh
+#chmod +x scripts/goodies/kernelsu.sh
+#source scripts/goodies/kernelsu.sh
 
 # Baseband Guard
 chmod +x scripts/goodies/baseband.sh
@@ -17,12 +17,12 @@ chmod +x scripts/goodies/nomount.sh
 source scripts/goodies/nomount.sh
 
 # Droidspaces
-chmod +x scripts/goodies/droidspaces.sh
-source scripts/goodies/droidspaces.sh
+#chmod +x scripts/goodies/droidspaces.sh
+#source scripts/goodies/droidspaces.sh
 
 # ReKernel
-chmod +x scripts/goodies/rekernel.sh
-source scripts/goodies/rekernel.sh
+#chmod +x scripts/goodies/rekernel.sh
+#source scripts/goodies/rekernel.sh
 
 # Misc
 chmod +x scripts/goodies/misc.sh
